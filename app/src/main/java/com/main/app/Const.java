e252b9ed1926b55d7c304160ab2191bc55833840
peoce <matthew98981@gmail.com>
2014-12-26 12:05:26 +0800

    add Const.java

---
+++ app/src/main/java/com/main/app/Const.java
@@ -0,0 +1,29 @@
+package com.main.app;
+
+/**
+ * Created by wangxm-wr on 2014/12/26 in Vanke.
+ */
+public class Const {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String PACKAGE_NAME = "com.way.weather";
+    public static final String BUILD_TYPE = "debug";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 2;
+    public static final String VERSION_NAME = "";
+
+    public static final String HOST="http://10.39.34.234";
+    public static final String PORT = ":41880/";
+    public static final String WEATHER = "Weather.html";
+    public static final String SIMPLE_WEATHER = "SimpleWeather.html";
+    public static final String AIRCONDITION = "AirCondition.html";
+    public static final String AIRBALL = "AirBallInfo.html";
+    public static final String RECORD = "record.html";
+
+    public static final String TAG_HOST_GET="http://10.39.34.31:9990/GetSignalData";
+    public static final String TAG_HOST_SET="http://10.39.34.31:9990/SetSignalData";
+    public static final String TAG_KEY = "e9c009aa-01f4-0082-00e6-1121629ea2a7";
+    public static final String TAG_EQUIPMENTID = "f8b279aa-e5f4-4082-80e6-0031629ea2a6";
+    public static final String TAG_SIGNALID = "DO0";
+
+}
+
